bcc1fd572990da30e11f10b68b270664d5317dcc
oleg <oleg.kontsograda@sperrodigital.com>
2024-11-29 08:45:26 -0500

    updating

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -1,22 +1,33 @@
 class_name State_Attack extends State
 
-@onready var walk = $"../Walk"
+var attacking : bool = false
+
+@onready var walk : State = $"../Walk"
+@onready var animation_player : AnimationPlayer = $"../../AnimationPlayer"
+@onready var idle  : State = $"../Idle"
 
 ## What happens when the player enters this State
 func Enter() -> void:
 	print("Attacking...")
 	player.UpdateAnimation("attack")
+	animation_player.animation_finished.connect( EndAttack )
+	attacking = true
 	pass
 	
 ## What happens when the player exists this state
 func Exit() -> void:
+	animation_player.animation_finished.disconnect( EndAttack )
+	attacking = false
 	pass
 
 ## What happens during the _process update in this state
 func Process ( _delta : float ) -> State:
-	if player.direction != Vector2.ZERO:
-		return walk
 	player.velocity = Vector2.ZERO
+	if attacking == false:
+		if player.direction == Vector2.ZERO:
+			return idle
+		else:
+			return walk
 	return null
 
 ## What happens during the _physics_process update in this state
@@ -26,3 +37,6 @@ func Physics( _delta : float ) -> State:
 ## What happens with input events in this state
 func HandleInput( _event : InputEvent ) -> State:
 	return null
+	
+func EndAttack( _newAnimName : String ) -> void:
+	attacking = false
