c89cc500737c1ee50deb6fcb8a7696c18efbde0a
okontsograda <oleg.kontsograda@gmail.com>
2024-11-29 12:48:35 -0500

    adding plants, hitboxes, etc.

---
+++ GeneralNodes/Hitbox/hit_box.gd
@@ -0,0 +1,15 @@
+class_name HitBox extends Area2D
+
+signal Damaged( damage : int )
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func TakeDamage( damage : int ) -> void:
+	print( "Damage taken: ", damage )
+	Damaged.emit( damage )
--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,11 +1,14 @@
 class_name Player extends CharacterBody2D
 
 var direction: Vector2 = Vector2.ZERO
+const DIR_4 = [ Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP ]
 var cardinal_direction: Vector2 = Vector2.DOWN
 
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 @onready var sprite : Sprite2D = $Sprite2D
 @onready var state_machine : PlayerStateMachine = $StateMachine
+
+signal DirectionChanged( new_direction : Vector2 )
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,20 +33,20 @@ func _physics_process(delta):
 	move_and_slide()
 
 func SetDirection() -> bool:
-	var new_dir : Vector2 = cardinal_direction
-	
 	if direction == Vector2.ZERO:
 		return false
 	
-	if direction.y == 0:
-		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
-	elif direction.x == 0:
-		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	var direction_id : int = int( round( ( direction + cardinal_direction * 0.1 ).angle() / TAU * DIR_4.size() ) )
+	var new_dir = DIR_4[ direction_id ]
 	
-	if new_dir == cardinal_direction:
+	# If direction doesn't change return
+	if new_dir == cardinal_direction || new_dir	 == Vector2.ZERO:
 		return false	
 	
 	cardinal_direction = new_dir
+	DirectionChanged.emit( new_dir )
+	
+	# Flip the sprite if moving left, otherwise ensure it's not flipped
 	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true
 	
